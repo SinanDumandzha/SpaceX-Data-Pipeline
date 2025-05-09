08ef6a66d1c63295ef80e1434da9e6fd9f229fe9
Sinan Dumandzha <sinan.dumanci@gmail.com>
2025-05-09 18:16:05 +0300

    Add hello.py and variables.py files

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Number of launches
+total_count = 10
+
+# Name of the rocket
+rocket_name = "Falcon 9"
+
+# Launch success rate
+success_rate = 0.95
+
+# Print variables
+print("Number of launches: ", total_count)
+print("Name of the rocket: ", rocket_name)
+print("Launch success rate: ", success_rate)
