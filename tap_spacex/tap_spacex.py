84afd797a58be123fb0e995973f86595daf8e83a
Sinan Dumandzha <sinan.dumanci@gmail.com>
2025-05-18 23:11:14 +0300

    Modify schema in tap_spacex.py file

--- tap_spacex/tap_spacex.py
+++ tap_spacex/tap_spacex.py
@@ -1,28 +1,37 @@
 import singer
 import pandas as pd
+import numpy as np
 
 LOGGER = singer.get_logger()
 
 schema = {
     "properties": {
-        "fairings": {"type": "null"},
+        "fairings": {
+            "type": ["object", "null"],
+            "properties": {
+                "reused": {"type": ["boolean", "null"]},
+                "recovery_attempt": {"type": ["boolean", "null"]},
+                "recovered": {"type": ["boolean", "null"]},
+                "ships": {"type": "array", "items": {"type": ["string", "null"]}},
+            },
+        },
         "links": {
             "type": "object",
             "properties": {
                 "patch": {
                     "type": "object",
                     "properties": {
-                        "small": {"type": "string"},
-                        "large": {"type": "string"},
+                        "small": {"type": ["string", "null"]},
+                        "large": {"type": ["string", "null"]},
                     },
                 },
                 "reddit": {
                     "type": "object",
                     "properties": {
-                        "campaign": {"type": "null"},
-                        "launch": {"type": "string"},
-                        "media": {"type": "null"},
-                        "recovery": {"type": "null"},
+                        "campaign": {"type": ["string", "null"]},
+                        "launch": {"type": ["string", "null"]},
+                        "media": {"type": ["string", "null"]},
+                        "recovery": {"type": ["string", "null"]},
                     },
                 },
                 "flickr": {
@@ -32,63 +41,53 @@ schema = {
                         "original": {"type": "array", "items": {}},
                     },
                 },
-                "presskit": {"type": "null"},
-                "webcast": {"type": "string"},
-                "youtube_id": {"type": "string"},
-                "article": {"type": "null"},
-                "wikipedia": {"type": "string"},
+                "presskit": {},
+                "webcast": {"type": ["string", "null"]},
+                "youtube_id": {"type": ["string", "null"]},
+                "article": {},
+                "wikipedia": {"type": ["string", "null"]},
             },
         },
-        "static_fire_date_utc": {"type": "null"},
-        "static_fire_date_unix": {"type": "null"},
-        "net": {"type": "boolean"},
-        "window": {"type": "null"},
-        "rocket": {"type": "string"},
-        "success": {"type": "boolean"},
+        "static_fire_date_utc": {},
+        "static_fire_date_unix": {},
+        "net": {"type": ["boolean", "null"]},
+        "window": {},
+        "rocket": {"type": ["string", "null"]},
+        "success": {"type": ["boolean", "null"]},
         "failures": {"type": "array", "items": {}},
-        "details": {"type": "null"},
-        "crew": {
-            "type": "array",
-            "items": [
-                {"type": "string"},
-                {"type": "string"},
-                {"type": "string"},
-                {"type": "string"},
-            ],
-        },
+        "details": {},
+        "crew": {"type": "array", "items": {"type": "string"}},
         "ships": {"type": "array", "items": {}},
-        "capsules": {"type": "array", "items": [{"type": "string"}]},
-        "payloads": {"type": "array", "items": [{"type": "string"}]},
-        "launchpad": {"type": "string"},
-        "flight_number": {"type": "integer"},
-        "name": {"type": "string"},
-        "date_utc": {"type": "string"},
-        "date_unix": {"type": "integer"},
-        "date_local": {"type": "string"},
-        "date_precision": {"type": "string"},
-        "upcoming": {"type": "boolean"},
+        "capsules": {"type": "array", "items": {"type": "string"}},
+        "payloads": {"type": "array", "items": {"type": "string"}},
+        "launchpad": {"type": ["string", "null"]},
+        "flight_number": {"type": ["integer", "null"]},
+        "name": {"type": ["string", "null"]},
+        "date_utc": {"type": "string", "format": "date-time"},
+        "date_unix": {"type": ["integer", "null"]},
+        "date_local": {"type": ["string", "null"]},
+        "date_precision": {"type": ["string", "null"]},
+        "upcoming": {"type": ["boolean", "null"]},
         "cores": {
             "type": "array",
-            "items": [
-                {
-                    "type": "object",
-                    "properties": {
-                        "core": {"type": "string"},
-                        "flight": {"type": "integer"},
-                        "gridfins": {"type": "boolean"},
-                        "legs": {"type": "boolean"},
-                        "reused": {"type": "boolean"},
-                        "landing_attempt": {"type": "boolean"},
-                        "landing_success": {"type": "boolean"},
-                        "landing_type": {"type": "string"},
-                        "landpad": {"type": "string"},
-                    },
-                }
-            ],
+            "items": {
+                "type": "object",
+                "properties": {
+                    "core": {"type": ["string", "null"]},
+                    "flight": {"type": ["integer", "null"]},
+                    "gridfins": {"type": ["boolean", "null"]},
+                    "legs": {"type": ["boolean", "null"]},
+                    "reused": {"type": ["boolean", "null"]},
+                    "landing_attempt": {"type": ["boolean", "null"]},
+                    "landing_success": {"type": ["boolean", "null"]},
+                    "landing_type": {"type": ["string", "null"]},
+                    "landpad": {"type": ["string", "null"]},
+                },
+            },
         },
-        "auto_update": {"type": "boolean"},
-        "tbd": {"type": "boolean"},
-        "launch_library_id": {"type": "string"},
+        "auto_update": {"type": ["boolean", "null"]},
+        "tbd": {"type": ["boolean", "null"]},
+        "launch_library_id": {"type": ["string", "null"]},
         "id": {"type": "string"},
     }
 }
@@ -97,6 +96,9 @@ schema = {
 def main():
     url = "https://api.spacexdata.com/v4/launches"
     df = pd.read_json(url)
+
+    df = df.replace({np.nan: None})
+    df["success"] = df["success"].apply(lambda x: bool(x) if pd.notnull(x) else None)
 
     records = df.to_dict(orient="records")
 
